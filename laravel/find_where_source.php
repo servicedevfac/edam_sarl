7882eff7b1a884fecbb5e6714502f49aecf089ac
servicedevfac <servicedevfac@gmail.com>
2026-04-07 15:52:05 +0000

    Nettoyage complet de la branche develop

--- laravel/find_where_source.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$r = new ReflectionMethod('Illuminate\Database\Query\Builder', 'where');
-echo "File: " . $r->getFileName() . "\n";
-echo "Start Line: " . $r->getStartLine() . "\n";
-echo "End Line: " . $r->getEndLine() . "\n";
